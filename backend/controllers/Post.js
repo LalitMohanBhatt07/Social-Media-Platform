729e5721f26c7fec0622e894b116ff0bfe6e17f5
Lb181808 <lb181808@gmail.com>
2024-08-15 18:18:46 +0530

    Created handler function to fetch all posts

--- backend/controllers/Post.js
+++ backend/controllers/Post.js
@@ -53,3 +53,30 @@ export const addNewPost=async(req,res)=>{
     }
 
 }
+
+export const getAllPost=async(req,res)=>{
+    try{
+        const posts=await Post.find().sort({createdAt:-1}).populate({path:'author',select:'userName,profilePicture'})
+        .populate({
+            path:'comments',
+            sort:{createdAt:-1}
+            .populate({
+                path:'author',
+                select:'userName,profilePicture'
+            })
+        })
+
+        return res.status(200).json({
+            success:true,
+            message:"Succesfully got all the post",
+            posts
+        })
+    }
+    catch(err){
+        return res.status(500).json({
+            success:false,
+            message:"Cannot get all Posts",
+            err
+        })
+    }
+}
