d39e5e25bc986e2072b3d326e9d52abedb3b54b1
Lb181808 <lb181808@gmail.com>
2024-08-17 14:14:42 +0530

    Created Component of Feed

--- frontend/src/components/ui/Feed.jsx
+++ frontend/src/components/ui/Feed.jsx
@@ -4,7 +4,10 @@ import Posts from './Posts'
 const Feed = () => {
   return (
     <div className='flex-1 my-8 flex flex-col items-center pl-[20%]'>
-      <Posts/>
+    {
+    [1,2,3,4].map((item,index)=><Posts key={index}/>)
+    }
+      
     </div>
   )
 }
--- frontend/src/components/ui/Posts.jsx
+++ frontend/src/components/ui/Posts.jsx
@@ -1,11 +1,22 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+
 import React from 'react'
 
 const Posts = () => {
   return (
-    <div>
+    <div className="my-8 w-full mx-w-sm mx-auto">
+    <div className="flex items-center justify-between">
+    <div className="flex items-center">
+    <Avatar>
+        <AvatarImage src="" alt="post_image" />
+        <AvatarFallback>CN</AvatarFallback>
+      </Avatar>
+      <h1>Username</h1>
+    </div>
+    </div>
       
     </div>
-  )
+  );
 }
 
 export default Posts
