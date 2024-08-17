33ca8420acc93c83e8f67b31777aad1e7f54121d
Lb181808 <lb181808@gmail.com>
2024-08-17 11:31:59 +0530

    Created left sidebar section

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import Feed from './ui/Feed'
+import { Outlet } from 'react-router-dom'
+import RightSidebar from './ui/RightSidebar'
 
 const Home = () => {
   return (
-    <div>
-      hello
+    <div className='flex'>
+    <div className="flex-grow">
+      <Feed/>
+      <Outlet/>
+    </div>
+      <RightSidebar/>
     </div>
   )
 }
